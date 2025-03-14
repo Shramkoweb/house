46fab298d43061d3b6661b722dd1e867c949e4db
Serhii Shramko <shramko.web@yahoo.com>
2025-03-14 18:38:53 -0400

    feat(database): add types and update connectDatabase return type

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,9 +1,10 @@
 import { MongoClient } from "mongodb";
+import { Database } from "../lib/types";
 
 const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net`;
 
 // TODO: add process.env validation
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
     const client = new MongoClient(url);
     await client.connect();
     const db = client.db(process.env.DB_NAME);
